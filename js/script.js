d4b6ee193918a830687e52fe0f512c47a32237bd
fahim <nafisbinnizam@gmail.com>
2025-06-25 22:33:56 +0600

    can't select theme element

--- js/script.js
+++ js/script.js
@@ -7,4 +7,27 @@
 
 // light and dark mode
 
-const 
+const theme_btn = document.querySelector('.icon-btn.theme-btn')
+
+const $HTML = document.documentElement;
+
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+
+const changeTheme = () => {
+
+  $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+
+  sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+
+theme_btn.addEventListener("click", changeTheme);
